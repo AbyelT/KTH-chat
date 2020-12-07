d434ddb20b6649c17296dec0ec787aaae3857cc5
abyel <Abyel@kth.se>
2020-12-07 15:52:42 +0100

    First commit

---
+++ src/main/java/com/natprogg/KTHchatapp/KthChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.natprogg.KTHchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KthChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KthChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natprogg/KTHchatapp/KthChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natprogg.KTHchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KthChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
