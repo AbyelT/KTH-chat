9fee1d721891b791d0c51c9a15a0ecc8d1b25610
erber014 <erikb2@kth.se>
2020-12-14 11:32:30 +0100

    Added keystore for https

--- src/main/java/com/natprogg/KTHchatapp/UserRepository.java
+++ src/main/java/com/natprogg/KTHchatapp/UserRepository.java
@@ -8,9 +8,13 @@ package com.natprogg.KTHchatapp;
 import org.springframework.data.repository.CrudRepository;
 
 import com.natprogg.KTHchatapp.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
